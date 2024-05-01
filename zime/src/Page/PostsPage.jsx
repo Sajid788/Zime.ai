bd551059ad98472cb4e7d57d6796010af50a0b32
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-05-01 21:03:39 +0530

    paginination and selector stracture created

--- zime/src/Page/PostsPage.jsx
+++ zime/src/Page/PostsPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Table, Input, Select, Pagination } from 'antd';
+import { motion } from "framer-motion";
 
 const { Search } = Input;
+const { Option } = Select;
 
 function PostsPage() {
     const columns = [
@@ -29,18 +31,54 @@ function PostsPage() {
     
   return (
     <>
-    <section className="bg-gray-100">
+     <section className="bg-gray-100">
         <div className="container mx-auto p-4 sm:p-8">
-          <h1 className="text-3xl font-bold text-gray-600 mb-4">
-          Discover Great Posts 
-          </h1>
-          <div className="flex flex-col sm:flex-row items-center justify-between mb-4">
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            className="text-3xl font-bold text-gray-600 mb-4"
+          >
+            Discover Great Posts
+          </motion.h1>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.4, duration: 0.5 }}
+            className="flex flex-col sm:flex-row items-center justify-between mb-4"
+          >
             <Search
               placeholder="Search posts"
               className="w-full max-w-md mb-4 sm:mb-0 sm:mr-4 rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:border-blue-500"
             />
-            <Table columns={columns} />
-          </div>
+            <Select
+              mode="multiple"
+              style={{ minWidth: 200 }}
+              placeholder="Select tags"
+              className="w-full sm:w-auto rounded-lg border border-gray-300 px-2 py-2 focus:outline-none focus:border-blue-500"
+            >
+            </Select>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            className="table-container overflow-x-auto rounded-lg shadow-lg"
+          >
+            <Table
+              columns={columns}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.8, duration: 0.5 }}
+            className="flex justify-center mt-4"
+          >
+            <Pagination
+              className="text-blue-600"
+            />
+          </motion.div>
         </div>
       </section>
     </>
