507efe8563003ed2492aa86ec5fcb4e8f3b527b1
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-05-03 10:39:29 +0530

    css add and Readme updated

--- zime/src/Page/PostsPage.jsx
+++ zime/src/Page/PostsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Input, Select, Pagination } from "antd";
+import { Table, Input, Select, Pagination,Tag } from "antd";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -9,117 +9,121 @@ const { Search } = Input;
 const { Option } = Select;
 
 function PostsPage() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [pagination, setPagination] = useState({
-      current: 1,
-      limit: 10,
-      total: 0,
-    });
-    const [searchQuery, setSearchQuery] = useState("");
-    const [selectedTags, setSelectedTags] = useState([]);
-    const [allTags, setAllTags] = useState([]);
-  
-    useEffect(() => {
-      fetchData();
-    }, [location]);
-  
-    useEffect(() => {
-      const delaySearch = setTimeout(() => {
-        fetchData();
-      }, 500);
-      return () => clearTimeout(delaySearch);
-    }, [searchQuery, pagination.current, pagination.limit]);
-  
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get(
-          `https://dummyjson.com/posts/search?q=${searchQuery}&skip=${
-            (pagination.current - 1) * pagination.limit
-          }&limit=${pagination.limit}`
-        );
-        const { posts, total } = response.data;
-        setPosts(posts);
-        setPagination((prevPagination) => ({
-          ...prevPagination,
-          total,
-        }));
-        const tags = posts.reduce((acc, post) => {
-          post.tags.forEach((tag) => {
-            if (!acc.includes(tag)) {
-              acc.push(tag);
-            }
-          });
-          return acc;
-        }, []);
-        setAllTags(tags);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-  
-    const handlePaginationChange = (page, limit) => {
-      setPagination((prevPagination) => ({
-        ...prevPagination,
-        current: page,
-        limit,
-      }));
-      navigate(`?page=${page}`);
-    };
-  
-    const handleSearch = (value) => {
-      setSearchQuery(value);
-      setPagination((prevPagination) => ({
-        ...prevPagination,
-        current: 1, 
-      }));
-      navigate(`?page=1&search=${value}`);
-    };
-  
-    const handleTagChange = (value) => {
-      setSelectedTags(value);
-      setPagination((prevPagination) => ({
-        ...prevPagination,
-        current: 1, 
-      }));
-      navigate(`?page=1&tags=${value.join(",")}`);
-    };
-  
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    limit: 10,
+    total: 0,
+  });
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [allTags, setAllTags] = useState([]);
 
-    const columns = [
-        {
-          title: "ID",
-          dataIndex: "id",
-          key: "id",
-        },
-        {
-          title: "Title",
-          dataIndex: "title",
-          key: "title",
-          width: "22%",
-          className: "text-gray-600 font-semibold",
-        },
-        {
-          title: "Body",
-          dataIndex: "body",
-          width: "60%",
-        },
-        {
-            title: "Tags",
-            dataIndex: "tags",
-            key: "tags",
-            responsive: ["lg"],
-          render: (tags) => (
-            <span className="text-green-600 capitalize">{tags.join(", ")}</span>
-          ),
-        },
-      ];
+  useEffect(() => {
+    fetchData();
+  }, [location]);
+
+  useEffect(() => {
+    const delaySearch = setTimeout(() => {
+      fetchData();
+    }, 500);
+    return () => clearTimeout(delaySearch);
+  }, [searchQuery, pagination.current, pagination.limit]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://dummyjson.com/posts/search?q=${searchQuery}&skip=${
+          (pagination.current - 1) * pagination.limit
+        }&limit=${pagination.limit}`
+      );
+      
+      const { posts, total } = response.data;
+      setPosts(posts);
+      setPagination((prevPagination) => ({
+        ...prevPagination,
+        total,
+      }));
+      const tags = posts.reduce((acc, post) => {
+        post.tags.forEach((tag) => {
+          if (!acc.includes(tag)) {
+            acc.push(tag);
+          }
+        });
+        return acc;
+      }, []);
+      setAllTags(tags);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  const handlePaginationChange = (page, limit) => {
+    setPagination((prevPagination) => ({
+      ...prevPagination,
+      current: page,
+      limit,
+    }));
+    navigate(`?page=${page}`);
+  };
+
+  const handleSearch = (value) => {
+    setSearchQuery(value);
+    setPagination((prevPagination) => ({
+      ...prevPagination,
+      current: 1,
+    }));
+    navigate(`?page=1&search=${value}`);
+  };
+
+  const handleTagChange = (value) => {
+    setSelectedTags(value);
+    setPagination((prevPagination) => ({
+      ...prevPagination,
+      current: 1,
+    }));
+    navigate(`?page=1&tags=${value.join(",")}`);
+  };
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      width: "22%",
+      className: "text-gray-600 font-semibold",
+    },
+    {
+      title: "Body",
+      dataIndex: "body",
+      width: "60%",
+    },
     
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      key: "tags",
+      responsive: ["lg"],
+      render: (tags) => (
+        <>
+          {tags.map((tag) => (
+            <Tag color="blue" key={tag}>{tag.toUpperCase()}</Tag>
+          ))}
+        </>
+      ),
+    },
+  ];
 
   const filteredPosts = selectedTags.length
     ? posts.filter((post) =>
@@ -146,11 +150,12 @@ function PostsPage() {
             className="flex flex-col sm:flex-row items-center justify-between mb-4"
           >
             <Search
-              placeholder="Search posts"
+              placeholder="input search text"
               onSearch={handleSearch}
+              enterButton
               onChange={(e) => setSearchQuery(e.target.value)}
               value={searchQuery}
-              className="w-full md:w-auto mb-4 sm:mb-0 sm:mr-4 rounded-lg px-2 y-2 focus:outline-none focus:border-blue-500"
+              className="w-full md:w-auto mb-4 sm:mb-0 sm:mr-4 rounded-lg px-2 y-2 "
             />
             <Select
               mode="multiple"
@@ -173,12 +178,12 @@ function PostsPage() {
             transition={{ delay: 0.6, duration: 0.5 }}
             className="table-container overflow-x-auto rounded-lg shadow-lg"
           >
-            <Table 
-             dataSource={filteredPosts}
-             columns={columns}
-             loading={loading}
-             pagination={false}
-             style={{ width: '100%' }}
+            <Table
+              dataSource={filteredPosts}
+              columns={columns}
+              loading={loading}
+              pagination={false}
+              style={{ width: "100%" }}
             />
           </motion.div>
           <motion.div
@@ -187,12 +192,12 @@ function PostsPage() {
             transition={{ delay: 0.8, duration: 0.5 }}
             className="flex justify-center mt-4"
           >
-            <Pagination 
-            current={pagination.current}
-            pageSize={pagination.pageSize}
-            total={pagination.total}
-            onChange={handlePaginationChange}
-            className="text-blue-600" 
+            <Pagination
+              current={pagination.current}
+              pageSize={pagination.pageSize}
+              total={pagination.total}
+              onChange={handlePaginationChange}
+              className="text-blue-600"
             />
           </motion.div>
         </div>
